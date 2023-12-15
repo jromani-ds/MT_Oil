0923626025f46cf86b48557f571d274b6a9026ea
Joe Romani <joseph.romani@gmail.com>
2023-12-14 20:26:49 -0700

    update Datacollection vignete and add stub for modelling

---
+++ prod_model.py
@@ -0,0 +1,21 @@
+# stub for ml model pipeline
+
+import pandas as pd
+
+def preprocess_well_data():
+    # gets us the stuff we need to know about the well
+    # to predict the production
+
+    # want |API | lat length | formation | 
+
+
+    return
+
+def preprocess_prod():
+    #allows us to preprocess the production data
+
+
+    return
+
+def model_pipeline():
+    #placeholder for the ml model pipeline
